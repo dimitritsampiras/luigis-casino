5804ba0c61e5ef29ee8cb90db37f8ce7f8290a3c
Dimitri Tsampiras <dimitri.tsampiras@gmail.com>
2021-03-31 16:38:17 -0400

    restructures css files

--- src/components/PicturePoker.js
+++ src/components/PicturePoker.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { bestHand } from '../utils/bestHand';
 import randomCard from '../utils/randomCard';
 import { Card } from './Card';
+import '../css/PicturePoker.css'
 
 export const PicturePoker = (props) => {
   const initialGameState = {
